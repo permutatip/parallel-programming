3702721cbc0e5950744493aa550096f882d1b8b7
permutatip <1740397484@qq.com>
2023-03-06 11:17:53 +0800

    modify vector

--- hw1/vector.c
+++ hw1/vector.c
@@ -8,6 +8,7 @@
 
 int n;
 int arr[MAX_SIZE],result[MAX_SIZE];
+int result2[MAX_SIZE];
 int mat[MAX_SIZE][MAX_SIZE];
 //----linux timer----
 #ifdef __linux
@@ -57,13 +58,13 @@ void cache_opt_mul()
     set_time(tm_start);
     for(int i=0;i<n;i++)
     {
-        result[i]=0;
+        result2[i]=0;
     }
     for(int j=0;j<n;j++)
     {
         for(int i=0;i<n;i++)
         {
-            result[i]+=mat[j][i]*arr[j];
+            result2[i]+=mat[j][i]*arr[j];
         }
     }
     set_time(tm_end);
@@ -97,5 +98,14 @@ int main(int argc,char** argv)
     
     traditional_mul();
     cache_opt_mul();
+
+    for(int i=0;i<n;i++)
+    {
+        if(result[i]!=result2[i])
+        {
+            printf("result diff at %d\n",i);
+        }
+    }
+
     return 0;
 }
